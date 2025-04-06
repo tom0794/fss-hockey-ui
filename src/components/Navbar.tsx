7fecd3b455cc5acf91bb2f4e2828f077f0025a9a
Tom Chiasson <thom.chiasson@gmail.com>
2025-04-06 18:28:14 -0400

    Get pages and chakra working

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,19 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { Box, Flex, Link as ChakraLink, Spacer, Heading } from "@chakra-ui/react"
+import { Link } from "react-router-dom"
 
-const Navbar: React.FC = () => {
+export default function Navbar() {
   return (
-    <nav style={{ padding: '1rem', background: '#f4f4f4' }}>
-      <Link to="/" style={{ marginRight: '1rem' }}>Home</Link>
-      <Link to="/players" style={{ marginRight: '1rem' }}>Players</Link>
-      <Link to="/teams">Teams</Link>
-    </nav>
+    <Box bg="blue.500" color="white" px={6} py={4} boxShadow="sm">
+      <Flex align="center">
+        <Heading size="md">
+          <Link to="/">FSS Hockey</Link>
+        </Heading>
+        <Spacer />
+        <Flex gap={4}>
+          <ChakraLink as={Link} to="/">Home</ChakraLink>
+          <ChakraLink as={Link} to="/teams">Teams</ChakraLink>
+        </Flex>
+      </Flex>
+    </Box>
   )
 }
-
-export default Navbar
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react"
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <ChakraProvider value={defaultSystem}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+    </ChakraProvider>
+  </React.StrictMode>,
 )
