2cadddba709fbb199c837ea6c3f95621188e626f
Tom Chiasson <thom.chiasson@gmail.com>
2025-04-07 20:58:59 -0400

    Remove proxy in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,8 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
-
-  const backendHost = env.BACKEND_HOST || 'http://localhost'
-  const backendPort = env.BACKEND_PORT || '8080'
-  const backendUrl = `${backendHost}:${backendPort}`
-  console.log(`[vite] Proxy target: ${backendUrl}`)
-
+export default defineConfig(({}) => {
   return {
-    plugins: [react()],
-    server: {
-      proxy: {
-        '/api': {
-          target: backendUrl,
-          changeOrigin: true,
-          rewrite: path => path.replace(/^\/api/, '')
-        }
-      }
-    }
+    plugins: [react()]
   }
 })
