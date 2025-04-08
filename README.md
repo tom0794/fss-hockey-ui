cbc6e6126a6cf7472f4343931753d58d571b8f29
Tom Chiasson <thom.chiasson@gmail.com>
2025-04-07 21:35:11 -0400

    Workaround to inject BACKEND_URL env var at runtime

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # fss-hockey-ui
 A frontend for Fantasy Sports Simulator: Hockey.
+
+## Container notes
+- Start backend container with `FRONTEND_URL` if different than default `http://host.docker.internal:8082` or `http://localhost:8082`
+- Start frontend container with env var `BACKEND_URL` set to `http://host.docker.internal:8080` (or whatever port backend container is running on)
--- src/api.ts
+++ src/api.ts
@@ -1,14 +1,13 @@
 // src/api.ts
 import axios from 'axios'
 
-const BASE_URL = window.__RUNTIME_API_URL__ || 'http://localhost:8080'
+const BASE_URL = (window.__RUNTIME_API_URL__ !== '{{BACKEND_URL}}' && window.__RUNTIME_API_URL__) || 'http://localhost:8080'
 
 export const apiClient = axios.create({
   baseURL: BASE_URL,
-  withCredentials: true // optional: include if you’re using cookies or auth
+  withCredentials: true
 })
 
-// Example helper
 export const endpointGet = async (endpoint: string) => {
   const res = await apiClient.get(endpoint)
   return res.data
